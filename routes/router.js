2b4d4e798b50da9c19e5579656d424d37368c9ca
rojasedwin <rojaedwin@gmail.com>
2022-03-12 21:43:14 -0400

    Avances

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const conexion  = require('../database/db');
+
 router.get('/', (req, res) => {
+    conexion()
     res.render('index');
-    //res.send('PANTALLA PRINCIPAL');
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
 
 module.exports = router;
