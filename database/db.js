64437750943ba3d34f348a3a395d67a7c61756b0
rojasedwin <rojaedwin@gmail.com>
2022-03-14 22:33:14 -0400

    Avances

--- database/db.js
+++ database/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-const conexion  = mysql.createConnection({
+/*const conexion  = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
     password: '', 
     database: 'sianew'
-})
+})*/
 
-/*const conexion  = mysql.createConnection({
+const conexion  = mysql.createConnection({
     host: 'us-cdbr-iron-east-01.cleardb.net', 
     user: 'b84edc25641db3', 
     password: '36094175', 
     database: 'heroku_55e1d1d5ed983d2'
-})*/
+})
 
 
 conexion.connect( (error) =>{
